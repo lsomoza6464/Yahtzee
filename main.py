9d059ac6cda0328de78c00630dcaf94397c36c8e
Logan Somoza <logansomoza@gmail.com>
2025-12-26 02:43:09 -0800

    Added functionality for ending the game and edgecases for things like multi-yahtzees and added ability to make scoresheet choices

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from table import Table
 
 
 YAHTZEE_CHOICES_PATH = "choices.json"
+TEST_ROLL_BOOL = True
+TEST_ROLLS = []
 
 def display_table():
     if os.path.exists(YAHTZEE_CHOICES_PATH):
@@ -29,20 +31,41 @@ def display_table():
                 #print("------------------------")
             print("------------------------")
 
-def roll(kept_rolls):
+def roll(kept_rolls): # add boolean that if TEST_ROLL_BOOL is true, you can add your own rolls instead of random rolls
+    if TEST_ROLL_BOOL:
+        return [1, 1, 1, 1, 1]
     for i in range(len(kept_rolls)):
         if kept_rolls[i] is None:
             kept_rolls[i] = random.randint(1, 6)
     return kept_rolls
 
+def scoresheet_choice(options, table: Table, rolls):
+    if options is not None:
+        print(f"Your scoresheet choice must be one of these options: {options}")
+    selection = input("What scoresheet choice do you want to use for this turn? (ex: select '7' or '3_of_a_kind' to choose '3 of a kind'): ")
+    validity = table.check_selection(selection)
+    while validity == "invalid" or validity == "taken" or (options is not None and table.table[validity][0] not in options):
+        if validity == "invalid":
+            selection = input("That was an invalid input, please enter a valid input (ex: select '7' or '3_of_a_kind' to choose '3 of a kind'): ")
+            validity = table.check_selection(selection)
+        elif validity == "taken":
+            selection = input(f"{selection} was already taken, please enter one of these options, {options}: ")
+            validity = table.check_selection(selection)
+        else:
+            selection = input(f"That is not one of the options, please enter one of these options, {options}: ")
+            validity = table.check_selection(selection)
+    table.add_selection(selection, rolls)
+    
+
 def main():
     print("Welcome to Yahtzee")
     print("After each roll, you will be provided with the current turn and roll.")
     print("If you ever need help or instructions on the application, write 'help'.")
-    print("If you ever need rules for Yahtzee, you can visit this site: ''website'' or write 'rules'.")
+    print("If you ever need rules for Yahtzee, you can visit this site: https://winning-moves.com/images/YAHTZEERULES_2022.pdf or write 'rules'.")
     #print("If you want to display the current table, write 'table'")
     #display_table()
     table = Table()
+    #table.calculate_score()
     for turn in range(1, 14):
         table.display()
         kept_rolls = [None] * 5
@@ -52,11 +75,49 @@ def main():
             rolls = roll(kept_rolls)
             print(f"Your die results are: {rolls}")
             if roll_num < 3:
-                keep_choice = input("Which numbers do you want to keep? (format is '01001' if you want to keep the second and fifth die): ")
-                for i in range(len(keep_choice)):
-                    if keep_choice[i] == "0":
-                        kept_rolls[i] = None
-            keep_choice = input(f"What scoresheet choice do you")
+                keep_choice = input("Which numbers do you want to keep? (ex: format is '01001' if you want to keep the second and fifth die): ")
+                while keep_choice != "" and len(keep_choice) != 5: #may need to add checks for specifically 0s and 1s
+                    keep_choice = input("That is not the correct format, the format should be made up of 0s and 1s and be 5 characters long (ex: format is '01001' if you want to keep the second and fifth die): ")
+                if keep_choice == "":
+                    kept_rolls = [None] * 5
+                else:
+                    for i in range(len(keep_choice)):
+                        if keep_choice[i] == "0":
+                            kept_rolls[i] = None
+                
+        if len(set(kept_rolls)) == 1 and table.table[11][1]:
+            print("You scored another Yahtzee!")
+            if table.table[rolls[0] - 1][1] is None:
+                #selection = rolls[0] - 1
+                #print(f"Your joker selection was {table.table[rolls[0] - 1][0]}")
+                options = table.table[rolls[0] - 1][0]
+                scoresheet_choice(options, table, rolls)
+            elif table.table[6][1] is None or table.table[7][1] is None or table.table[8][1] is None:
+                options = []
+                for i in range(6, 9):
+                    if table.table[i][1] is None:
+                        options.append(table.table[i][0])
+                scoresheet_choice(options, table, rolls)
+            else:
+                options = []
+                for i in range(9, 13):
+                    if table.table[i][1] is None and i != 11: # skipping yahtzee
+                        options.append(table.table[i][0])
+                if not options:
+                    for i in range(1, 7):
+                        if table.table[i][1] is None:
+                            options.append(table.table[i][0])
+                scoresheet_choice(options, table, rolls)
+        else:
+            scoresheet_choice(None, table, rolls)
+                #selection = input("What scoresheet choice do you want to use for this turn? (ex: select '7' or '3_of_a_kind' to choose '3 of a kind'): ")
+                #while not table.add_selection(selection, kept_rolls):
+                #    selection = input("That was an invalid input, please enter a valid input (ex: select '7' or '3_of_a_kind' to choose '3 of a kind'): ")
+    print("You have finished your turns, here is your final scoresheet:")
+    table.display()
+    final_score, true_final_score = table.calculate_scores()
+    print(f"\nYour final score was {final_score}")
+    print(f"Your true score (without including 100 bonus per extra yahtzee) was {true_final_score}")
 
 if __name__ == "__main__":
     main()
--- table.py
+++ table.py
@@ -1,6 +1,6 @@
 class Table:
     def __init__(self):
-        self.table = {
+        self.dict = {
             "ones": None,
             "twos": None,
             "threes": None,
@@ -12,18 +12,35 @@ class Table:
             "full_house": None,
             "small_straight": None,
             "large_straight": None,
-            "yahtzee": None
+            "yahtzee": None,
+            "chance": None
         }
+        self.mapping = {
+            "ones": 0,
+            "twos": 1,
+            "threes": 2,
+            "fours": 3,
+            "fives": 4,
+            "sixes": 5,
+            "3_of_a_kind": 6,
+            "4_of_a_kind": 7,
+            "full_house": 8,
+            "small_straight": 9,
+            "large_straight": 10,
+            "yahtzee": 11,
+            "chance": 12
+        }
+        self.table = [["ones", None], ["twos", None], ["threes", None], ["fours", None], ["fives", None], ["sixes",None], ["3_of_a_kind", None], ["4_of_a_kind", None], ["full_house", None], ["small_straight", None], ["large_straight", None], ["yahtzee", None], ["chance", None]]
 
     def display(self):
-        items = list(self.table.items())
+        #items = list(self.dict.items())
         print("------------------------------")
-        for i in range(len(items)):
-            filler_key = len("small_straight") - len(items[i][0])
+        for i in range(len(self.table)):
+            filler_key = len("small_straight") - len(self.table[i][0])
             filler_key_str_l = " " * (filler_key // 2)
             filler_key_str_r = " " * ((filler_key + 1) // 2)
 
-            value = str(items[i][1])
+            value = str(self.table[i][1])
             if value == "None":
                 value = ""
             filler_value = len("999") - len(value)
@@ -34,5 +51,84 @@ class Table:
                 index_str_r = " "
             else:
                 index_str_r = ""
-            print(f"|  {i + 1}{index_str_r} | {filler_key_str_l}{items[i][0]}{filler_key_str_r} | {filler_val_str_l}{value}{filler_val_str_r} |")
+            print(f"|  {i + 1}{index_str_r} | {filler_key_str_l}{self.table[i][0]}{filler_key_str_r} | {filler_val_str_l}{value}{filler_val_str_r} |")
         print("------------------------------")
+
+    def add_selection(self, selection, rolls):
+        if selection.isnumeric() and int(selection) >= 1 and int(selection) <= len(self.table):
+            index = int(selection) - 1
+        elif selection in self.mapping:
+            index = self.mapping[selection]
+        else:
+            return ValueError("selection index not found")
+        value = self.get_roll_value(index, rolls)
+        self.table[index][1] = value
+    
+    def get_roll_value(self, index, rolls):
+        value = self.multi_yahtzee(index, rolls) # start value at the 100 of multi-yahtzee and add from there
+        if not self.zero_score(index, rolls):
+            if index < 6:
+                for roll in rolls:
+                    if roll == index + 1:
+                        value += roll
+            elif index == 6 or index == 7 or index == 12:
+                value += sum(rolls)
+            elif index == 8:
+                value += 25
+            elif index == 9:
+                value += 30
+            elif index == 10:
+                value += 40
+            elif index == 11:
+                value += 50
+        return value
+
+    def zero_score(self, index, rolls): # checks if the rolls actually are valid for each requirements
+        freqArr = [0] * 7
+        for roll in rolls:
+            freqArr[roll] += 1
+        freqSet = set(freqArr)
+        seq_length = self.len_longest_sequence(rolls)
+        if (index == 6 and (3 not in freqSet and 4 not in freqSet and 5 not in freqSet)) or (index == 7 and (4 not in freqSet and 5 not in freqSet)) or (index == 8 and (3 not in freqSet or 2 not in freqSet)) or (index == 9 and seq_length < 4) or (index == 10 and seq_length < 5) or (index == 11 and 5 not in freqSet):
+            return True
+        return False
+    
+    def len_longest_sequence(self, rolls):
+        seqSet = set(rolls)
+        count = 0
+        max_count = 0
+        for i in range(1, 7):
+            if i in seqSet:
+                count += 1
+            else:
+                count = 0
+            max_count = max(max_count, count)
+        return max_count
+
+    def multi_yahtzee(self, index, rolls):
+        if self.table[11] is not None and len(set(rolls)) == 1 and index != 11:
+            return 100
+        return 0
+    
+    def check_selection(self, selection):
+        if selection.isnumeric() and int(selection) >= 1 and int(selection) <= len(self.table):
+            index = int(selection) - 1
+        elif selection in self.mapping:
+            index = self.mapping[selection]
+        else:
+            return "invalid"
+        if self.table[index][1] is not None:
+            return "taken"
+        return index # kinda messy to return strings or ints
+    
+    def calculate_scores(self):
+        score_arr = list(map(lambda x: x[1], self.table))
+        true_score_arr = list(map(lambda x: x - 100 if x >= 100 else x))
+        upper_score = sum(score_arr[0:6])
+        true_upper_score = sum(true_score_arr[0:6])
+        if true_upper_score >= 63:
+            upper_score += 35
+            true_upper_score += 35
+        lower_score = sum(score_arr[6:len(score_arr)])
+        true_lower_score = sum(true_score_arr[6:len(true_score_arr)])
+        return [upper_score + lower_score, true_upper_score + true_lower_score]
